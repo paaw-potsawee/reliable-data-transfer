ea7cc8fc1a383e346dd68af76f70a8b71036145c
potsawee <potsaweee.p@gmail.com>
2026-03-14 00:07:18 +0700

    Implement UDP file transfer

--- urft_client.py
+++ urft_client.py
@@ -1,15 +1,127 @@
 import socket
 import sys
+import struct
+import time
+import os
+from typing import List
 
-UDP_IP = sys.argv[2] if len(sys.argv) > 1 else "127.0.0.1"
-UDP_PORT = int(sys.argv[3]) if len(sys.argv) > 2 else 5005
-MESSAGE = sys.argv[1]
+# python urft_client.py <file_path> <ip> <port>
+FILE_PATH = sys.argv[1] if len(sys.argv) > 1 else "test.bin"
+UDP_IP = sys.argv[2] if len(sys.argv) > 2 else "127.0.0.1"
+UDP_PORT = int(sys.argv[3]) if len(sys.argv) > 3 else 5005
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+class Sender:
+    def __init__(self):
+        self.__window_size = 1024 
+        self.__chunk_size = 1024
+        self.__chunks: List[bytes] = []
+        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        
+    def read_file(self, file_path):
+        try:
+            with open(file_path, 'rb') as f:
+                while True:
+                    data = f.read(self.__chunk_size)
+                    if not data:
+                        break
+                    self.__chunks.append(data)
+            print(f"Read '{file_path}': {len(self.__chunks)} chunks prepared.")
+            return True
+        except FileNotFoundError as e:
+            print(f"Error: {e}")
+            return False
+        
+    def create_packet(self, packet_type, seq_num, payload):
+        header = struct.pack('!BxxxI', packet_type, seq_num) 
+        return header + payload
+    
+    def send_metadata_with_ack(self, filename, timeout=1.0):
+        packet = self.create_packet(0x00, 0, filename.encode('utf-8')) 
+        
+        self.__sock.settimeout(timeout)
+        while True:
+            self.__sock.sendto(packet, (UDP_IP, UDP_PORT))
+            try:
+                ack_packet, _ = self.__sock.recvfrom(8)
+                ack_type = ack_packet[0]
+                ack_seq = struct.unpack('!BxxxI', ack_packet)[1]
+                
+                if ack_type == 0xFF and ack_seq == 0:
+                    return
+            except socket.timeout:
+                # timeout pass and send header again
+                pass
+            
+    def send_data_with_window(self, logical_timeout=0.5):
+        base = 1 
+        next_seq = 1     
+        packets_sent = {}
+        acked = set()
+        total_chunks = len(self.__chunks)
+        
+        # set little timeout
+        self.__sock.settimeout(0.02) 
 
-# send file name to server
-
-# send content of file to server
-sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
-
-sock.close()
+        while base <= total_chunks:
+            # send all data in window
+            while next_seq < base + self.__window_size and next_seq <= total_chunks:
+                chunk_idx = next_seq - 1 
+                packet = self.create_packet(0x01, next_seq, self.__chunks[chunk_idx])
+                
+                self.__sock.sendto(packet, (UDP_IP, UDP_PORT))
+                packets_sent[next_seq] = (packet, time.time())
+                next_seq += 1
+                
+            # wait for ack
+            try:
+                ack_packet, _ = self.__sock.recvfrom(8)
+                ack_type = ack_packet[0]
+                ack_seq = struct.unpack('!BxxxI', ack_packet)[1]
+                
+                if ack_type == 0xFF and ack_seq in packets_sent:
+                    del packets_sent[ack_seq]
+                    acked.add(ack_seq)
+                    
+                    # sliding window (Base) to latest ack
+                    while base in acked:
+                        base += 1
+            except socket.timeout:
+                pass
+            
+            # retranmission non-acked packets
+            current_time = time.time()
+            for seq_num, (packet, timestamp) in list(packets_sent.items()):
+                if current_time - timestamp > logical_timeout:
+                    self.__sock.sendto(packet, (UDP_IP, UDP_PORT))
+                    packets_sent[seq_num] = (packet, current_time)
+                    
+    def send_fin(self, timeout=1.0):
+        fin_seq = len(self.__chunks) + 1 
+        packet = self.create_packet(0x02, fin_seq, b'')
+        
+        self.__sock.settimeout(timeout)
+        while True:
+            self.__sock.sendto(packet, (UDP_IP, UDP_PORT))
+            try: 
+                ack_packet, _ = self.__sock.recvfrom(8)
+                ack_seq = struct.unpack('!BxxxI', ack_packet)[1]
+                if ack_seq == fin_seq:
+                    return
+            except socket.timeout:
+                # retransmition
+                pass
+        
+    def run(self):
+        if not self.read_file(FILE_PATH):
+            return
+            
+        start_time = time.time()
+        self.send_metadata_with_ack(os.path.basename(FILE_PATH))
+        self.send_data_with_window()
+        self.send_fin()
+        self.__sock.close()
+        print(f"Total time taken: {time.time() - start_time:.2f} seconds")
+        
+if __name__ == "__main__":
+    sender = Sender()
+    sender.run()
--- urft_server.py
+++ urft_server.py
@@ -1,20 +1,90 @@
 import socket
 import sys
+import struct
+import os
 
+# python urft_server.py <file_path> <ip> <port>
 UDP_IP = sys.argv[1] if len(sys.argv) > 1 else "127.0.0.1"
 UDP_PORT = int(sys.argv[2]) if len(sys.argv) > 2 else 5005
-BUFFER_SIZE = 1024 # bytes
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+class Recipient:
+    def __init__(self):
+        self.__file_path = ''
+        self.__received_buff = {} 
+        self.__sock = None
+        
+    def receive_metadata(self, timeout=2):
+        self.__sock.settimeout(timeout)
+        while True:
+            try:
+                data, addr = self.__sock.recvfrom(4096)
+                packet_type = data[0]
+                seq_num = struct.unpack('!BxxxI', data[:8])[1]
+                
+                if packet_type == 0x00:
+                    raw_filename = data[8:].decode('utf-8').strip('\x00')
+                    self.__file_path = os.path.basename(raw_filename)
+                    
+                    print(f"Received Metadata. Filename: {self.__file_path}") 
 
-sock.bind((UDP_IP, UDP_PORT))
-print(f"UDP server listening on {UDP_IP}:{UDP_PORT}")
+                    ack = struct.pack('!BxxxI', 0xFF, seq_num)
+                    self.__sock.sendto(ack, addr)
+                    
+                    self.__sock.settimeout(None)
+                    return
+                    
+            except socket.timeout:
+                # waiting for meta bata from client
+                pass
+                
+    def receive_data_with_buffer(self):
+        next_expected = 1 
+        
+        with open(self.__file_path, 'wb') as f:
+            while True:
+                try: 
+                    data, addr = self.__sock.recvfrom(4096)
+                    packet_type = data[0]
+                    seq_num = struct.unpack('!BxxxI', data[:8])[1]
+                    payload = data[8:] 
+                    
+                    if packet_type == 0x02: # FIN 
+                        ack = struct.pack('!BxxxI', 0xFF, seq_num)
+                        self.__sock.sendto(ack, addr)
+                        print(f"File '{self.__file_path}' transfer complete!")
+                        break
 
-# get file name from client and create new file
+                    elif packet_type == 0x01: # Data
+                        ack = struct.pack('!BxxxI', 0xFF, seq_num)
+                        # send ack with seq_num
+                        self.__sock.sendto(ack, addr)
+                        
+                        # add to received buff if not expected sequence
+                        if seq_num >= next_expected and seq_num not in self.__received_buff:
+                            self.__received_buff[seq_num] = payload
+                        
+                        # if received buff is in correct order write to file
+                        while next_expected in self.__received_buff:
+                            f.write(self.__received_buff.pop(next_expected))
+                            next_expected += 1
+                            
+                    elif packet_type == 0x00: # re-acked meta data 
+                        ack = struct.pack('!BxxxI', 0xFF, 0)
+                        self.__sock.sendto(ack, addr)
+                        
+                except Exception as e:
+                    print(f"Error during file transfer: {e}")
+                    break
+                
+    def run(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((UDP_IP, UDP_PORT))
+        self.__sock = sock
+        
+        self.receive_metadata()
+        self.receive_data_with_buffer()
+        self.__sock.close()
 
-# get content of file from client
-while True:
-    data, addr = sock.recvfrom(BUFFER_SIZE)
-    print(f"Received message from {addr}: {data.decode()}")
-
-    # sock.senddto(b"Message Received", addr)
+if __name__  == "__main__":
+    recipient = Recipient()
+    recipient.run()
